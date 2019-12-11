339749e36b1295b90f1d6cf9f1f579ab5ca4a12e
Jackson  Ricks <jacksonricks@Jacksons-MacBook-Pro.local>
2019-12-10 23:03:02 -0800

    ok

---
+++ app/src/main/java/com/example/spohike/SingleTrail.java
@@ -0,0 +1,14 @@
+package com.example.spohike;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class SingleTrail extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_single_trail);
+    }
+}
